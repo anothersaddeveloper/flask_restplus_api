d966102189046bde6bcf1ab76f3d7f9d4e37ac53
Rafal Rosa <rrosa@poppulo.com>
2019-11-28 14:20:19 +0000

    Added blueprint to app env

--- manage.py
+++ manage.py
@@ -6,8 +6,13 @@ from flask_script import Manager
 
 from app.main import create_app, db
 from app.main.model import user_models
+from app import blueprint
 
 app = create_app(os.getenv('BOILERPLATE_ENV') or 'dev')
+
+app.register_blueprint(blueprint)
+
+app.app_context().push()
 
 app.app_context().push()
 
