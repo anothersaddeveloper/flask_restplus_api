cf5840939ea5b256fe9e0373da9e11e1848814cd
Rafal Rosa <rrosa@poppulo.com>
2019-11-28 14:21:33 +0000

    Changed the path, added title and description to swagger docs

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,15 @@
+
 from flask_restplus import Api
 from flask import Blueprint
 
-# from main.controller.user_controller import api as user_ns
-# from main.controller.auth_controller import api as auth_ns
+from .main.controller.rest_db_controller import api as user_ns
 
 blueprint = Blueprint('api', __name__)
 
 api = Api(blueprint,
-          title='FLASK RESTPLUS API BOILER-PLATE WITH JWT',
+          title='MEDI-AI API SWAGGER',
           version='1.0',
-          description='a boilerplate for flask restplus web service'
+          description='A Swagger interface with API examples for API calls and DTOs'
           )
 
-# api.add_namespace(user_ns, path='/user')
-# api.add_namespace(auth_ns)
+api.add_namespace(user_ns, path='/patient')
