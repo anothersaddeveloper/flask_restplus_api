e7d593ea8dd17834ed0a7970323f7a5ef3dc270a
Rafal Rosa <rrosa@poppulo.com>
2019-11-28 14:23:23 +0000

    Added patient DTO and rest controller

--- app/main/controller/rest_db_controller.py
+++ app/main/controller/rest_db_controller.py
@@ -1,37 +1,40 @@
 from flask import request
 from flask_restplus import Resource
-from app.main.model.user_models import Patient, Doctor
-from .. import db
 
-api = DoctorDto.api
-user_auth = AuthDto.user_auth
+from ..util.dto import PatientDto
+from ..service.user_service import save_new_user, get_all_users, get_a_user
+
+api = PatientDto.api
+_user = PatientDto.user
 
 
-@api.route('/login')
-class UserLogin(Resource):
-    """
-        User Login Resource
-    """
-    @api.doc('user login')
-    @api.expect(user_auth, validate=True)
+@api.route('/')
+class UserList(Resource):
+    @api.doc('list of all patients')
+    @api.marshal_list_with(_user, envelope='data')
+    def get(self):
+        """List all registered patients"""
+        return get_all_users()
+
+    @api.response(201, 'User successfully created.')
+    @api.doc('create a new patient')
+    @api.expect(_user, validate=True)
     def post(self):
-        # get the post data
-        post_data = request.json
-        return Auth.login_user(data=post_data)
+        """Creates a new User """
+        data = request.json
+        return save_new_user(data=data)
 
 
-@api.route('/register')
-class LogoutAPI(Resource):
-    """
-        Regsitering Resource
-    """
-    @api.doc('register a user')
-    def post(self):
-        doctor = Doctor(email="DoctorSmith@mail", username="dSmith")
-        db.session.add(doctor)
-        db.session.commit()
-        print(doctor)
-        print(db.session.query(Doctor).filter_by(id=1).all())
-        # get auth token
-        # auth_header = request.headers.get('Authorization')
-        # return Auth.logout_user(data=auth_header)
+@api.route('/<id>')
+@api.param('id', 'Patient ID')
+@api.response(404, 'User not found.')
+class User(Resource):
+    @api.doc('get a patient')
+    @api.marshal_with(_user)
+    def get(self, public_id):
+        """get a patient given its identifier"""
+        user = get_a_user(public_id)
+        if not user:
+            api.abort(404)
+        else:
+            return user
--- app/main/util/dto.py
+++ app/main/util/dto.py
@@ -10,7 +10,15 @@ class DoctorDto:
         'public_id': fields.String(description='user Identifier')
     })
 
-
+class PatientDto:
+    api = Namespace('user', description='user related operations')
+    user = api.model('user', {
+        'email': fields.String(required=True, description='user email address'),
+        'username': fields.String(required=True, description='user username'),
+        'password': fields.String(required=True, description='user password'),
+        'public_id': fields.String(description='user Identifier')
+    })
+    
 class AuthDto:
     api = Namespace('auth', description='authentication related operations')
     user_auth = api.model('auth_details', {
