24afbeb797ae043fcb88f69918a6c800ae36c79c
Rafal Rosa <rrosa@poppulo.com>
2019-12-02 21:31:52 +0000

    Added DTOs for medi-ai records

--- app/main/util/dto.py
+++ app/main/util/dto.py
@@ -32,43 +32,43 @@ class InsuranceProfessionalDto:
 class CancerRecordDto:
     api = Namespace('insurance_professional', description='insurance professional related operations')
     insurance_professional = api.model('insurance_professional', {
-        '`age`': fields.Integer(required=True, description='insurance professional email address'),
+        'age': fields.Integer(required=True, description='insurance professional email address'),
         'bmi': fields.Float(required=True, description='insurance professional username'),
-        'glucose': fields.String(required=True, description='insurance professional password'),
-        'insulin': fields.String(required=True, description='profession variable'),
-        'homa': fields.String(required=True, description='profession variable'),
-        'leptin': fields.String(required=True, description='profession variable'),
-        'adiponectin': fields.String(required=True, description='profession variable'),
-        'resistin': fields.String(required=True, description='profession variable')
-  
-        age = db.Column(db.Integer)
-        bmi = db.Column(db.Float)
-        glucose = db.Column(db.Float)
-        insulin = db.Column(db.Float)
-        homa = db.Column(db.Integer)
-        leptin = db.Column(db.Float)
-        adiponectin = db.Column(db.Integer)
-        resistin = db.Column(db.Integer)
-        mcp_1 = db.Column(db.Float)
-        patient_id
+        'glucose': fields.Integer(required=True, description='insurance professional password'),
+        'insulin': fields.Integer(required=True, description='profession variable'),
+        'homa': fields.Integer(required=True, description='profession variable'),
+        'leptin': fields.Integer(required=True, description='profession variable'),
+        'adiponectin': fields.Integer(required=True, description='profession variable'),
+        'resistin': fields.Integer(required=True, description='profession variable'),
+        'mcp_1': fields.Integer(required=True, description='profession variable')
     })
 
-class InsuranceProfessionalDto:
+class DiabetesRecordDto:
     api = Namespace('insurance_professional', description='insurance professional related operations')
     insurance_professional = api.model('insurance_professional', {
-        'email': fields.String(required=True, description='insurance professional email address'),
-        'username': fields.String(required=True, description='insurance professional username'),
-        'password': fields.String(required=True, description='insurance professional password'),
-        'profession': fields.String(required=True, description='profession variable')
+        'age': fields.Integer(required=True, description='insurance professional email address'),
+        'bmi': fields.Float(required=True, description='insurance professional username'),
+        'glucose': fields.Integer(required=True, description='insurance professional password'),
+        'insulin': fields.Integer(required=True, description='profession variable'),
+        'homa': fields.Integer(required=True, description='profession variable'),
+        'leptin': fields.Integer(required=True, description='profession variable'),
+        'adiponectin': fields.Integer(required=True, description='profession variable'),
+        'resistin': fields.Integer(required=True, description='profession variable'),
+        'mcp_1': fields.Integer(required=True, description='profession variable')
     })
 
-class InsuranceProfessionalDto:
+class HeartDiseaseDto:
     api = Namespace('insurance_professional', description='insurance professional related operations')
     insurance_professional = api.model('insurance_professional', {
-        'email': fields.String(required=True, description='insurance professional email address'),
-        'username': fields.String(required=True, description='insurance professional username'),
-        'password': fields.String(required=True, description='insurance professional password'),
-        'profession': fields.String(required=True, description='profession variable')
+        'age': fields.Integer(required=True, description='insurance professional email address'),
+        'bmi': fields.Float(required=True, description='insurance professional username'),
+        'glucose': fields.Integer(required=True, description='insurance professional password'),
+        'insulin': fields.Integer(required=True, description='profession variable'),
+        'homa': fields.Integer(required=True, description='profession variable'),
+        'leptin': fields.Integer(required=True, description='profession variable'),
+        'adiponectin': fields.Integer(required=True, description='profession variable'),
+        'resistin': fields.Integer(required=True, description='profession variable'),
+        'mcp_1': fields.Integer(required=True, description='profession variable')
     })
 
 class AuthDto:
